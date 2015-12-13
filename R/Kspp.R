9cfe04a5d77a3981c798f255451afcb94100a35e
rubak <rubak@math.aau.dk>
2015-12-13 22:59:43 +0100

    K function argument can be an angle rather than the corresponding great circle dist.

--- R/Kspp.R
+++ R/Kspp.R
@@ -13,32 +13,41 @@
 #'   future).
 #' @param lambda Intensity (usually not supplied). Estimated from data by default.
 #' @param unnormalized Logical. Only for internal use at the moment.
+#' @param angular_arg Logical. Interpret r values as angles (possibly in degrees
+#'   rather than radians depending on coordinate type of \code{X}).
 #' @return An object of class \code{"fv"}, see \code{\link{fv.object}}, which
 #'   can be plotted directly using \code{\link{plot.fv}}.
 #'
 #'   Essentially a data frame containing columns \item{r}{the vector of values
 #'   of the argument \eqn{r} at which the function \eqn{K} has been estimated }
 #'   \item{theo}{the theoretical value \eqn{K(r) = 2 \pi (1-cos(r))}{K(r) = 2 *
-#'   pi * (1 - cos(r))} for a stationary Poisson process } together with columns
-#'   named \code{"none"}, and/or \code{"border"}, according to the selected edge
-#'   corrections. These columns contain estimates of the function \eqn{K(r)}
-#'   obtained by the edge corrections named.
+#'   pi * (1 - cos(r))} for a stationary Poisson process (where r is in
+#'   radians)} together with columns named \code{"none"}, and/or
+#'   \code{"border"}, according to the selected edge corrections. These columns
+#'   contain estimates of the function \eqn{K(r)} obtained by the edge
+#'   corrections named.
 #' @export
 #'
 #' @examples
 #' X <- runifspp(100)
 #' K <- Kspp(X)
 #'
-Kspp <- function(X, r = NULL, rmax = NULL, breaks = NULL, correction = NULL, lambda = NULL, unnormalized = FALSE){
+Kspp <- function(X, r = NULL, rmax = NULL, breaks = NULL, correction = NULL,
+                 lambda = NULL, unnormalized = FALSE, angular_arg = FALSE){
   ratio <- FALSE # This could potentially be an argument of the function in the long run.
+
+  # Check whether r values should be degrees (radians will be used and the chaged to degrees at the end)
+  dom <- domain(X)
+  degrees <- angular_arg && grepl("deg", dom$coord_type)
+  if(!is.null(rmax) && degrees) rmax <- rmax*2*pi/360
+
   # Figure out r-values
   rmaxdefault <- rmax %orifnull% pi
-  breaks <- handle.r.b.args(r, breaks, domain(X), pixeps = rmaxdefault/128, rmaxdefault=rmaxdefault)
+  breaks <- handle.r.b.args(r, breaks, dom, pixeps = rmaxdefault/128, rmaxdefault=rmaxdefault)
   r <- breaks$r
   rmax <- breaks$max
 
   # Choose correction based on domain if unset by user
-  dom <- domain(X)
   best_correction <- ifelse(dom$type=="sphere", "none", "border")
   if(missing(correction) || is.null(correction)){
     correction <- best_correction
@@ -61,19 +70,20 @@ Kspp <- function(X, r = NULL, rmax = NULL, breaks = NULL, correction = NULL, lam
   theo <- 2*pi*(1-cos(r))
   if(unnormalized) theo <- lambda2 * theo
   Odf <- data.frame(r = r, theo = theo)
-  desc <- c("distance argument r",
-            "theoretical isotropic %s")
+  desc <- c(paste("distance argument", ifelse(!angular_arg, "r", "theta")),
+            "theoretical uncorrected %s")
+  ylab <- if(!angular_arg) quote(K(r)) else quote(K(theta))
   OO <- fv(Odf,
            argu = "r",
-           ylab = quote(K(r)),
+           ylab = ylab,
            valu = "theo",
            fmla = . ~ r,
-           alim = c(0, rmax),
-           labl = c("r",
-                    "%s[pois](r)"),
+           alim = c(0, ifelse(!degrees, rmax, rmax*360/(2*pi))),
+           labl = c(ifelse(!angular_arg, "r", "theta"),
+                    ifelse(!angular_arg, "%s[pois](r)", "%s[pois](theta)")),
            desc = desc,
            fname = "K",
-           yexp = quote(K(r))
+           yexp = ylab
   )
 
   delta <- pairdist(X)
@@ -86,8 +96,9 @@ Kspp <- function(X, r = NULL, rmax = NULL, breaks = NULL, correction = NULL, lam
     if(!unnormalized) khat <- khat/(lambda2*areaW)
 
     ## uncorrected estimate
+    labl <- ifelse(!angular_arg, "hat(%s)(r)", "hat(%s)(theta)")
     OO <- bind.fv(OO, data.frame(un=khat),
-                  "hat(%s)(r)",
+                  labl,
                   "uncorrected estimate of %s",
                   "un")
   }
@@ -109,14 +120,17 @@ Kspp <- function(X, r = NULL, rmax = NULL, breaks = NULL, correction = NULL, lam
       denKb / lambda
       numKb <- numKb * (n-1)
     }
+    labl <- ifelse(!angular_arg, "hat(%s)[bord](r)", "hat(%s)[bord](theta)")
     OO <- bind.ratfv(OO,
                      data.frame(border=numKb),
                      data.frame(border=denKb),
-                     "hat(%s)[bord](r)",
+                     labl,
                      "border-corrected estimate of %s",
                      "border",
                      ratio=ratio)
   }
+
+  if(degrees) OO$r <- OO$r*360/(2*pi)
 
   return(OO)
 }
